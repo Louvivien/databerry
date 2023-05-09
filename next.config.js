4f8e27efc6bb1c5e9e092f90e98e32a155b265f0
louvivien <vivien.richaud@gmail.com>
2023-05-09 16:56:00 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const baseConfig = require('./next.config.base');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   ...baseConfig,
+  experimental: {
+    legacyBrowsers: false,
+    outputFileTracingIgnores: ['**canvas**'],
+},
 };
 
 module.exports = nextConfig;
