00bcd8062e7e3cd5baa5e7dcf7644b0b9f0a0add
louvivien <vivien.richaud@gmail.com>
2023-05-11 13:00:35 +0200

    update aws

--- utils/aws.ts
+++ utils/aws.ts
@@ -4,7 +4,7 @@ export const s3 = new S3({
   signatureVersion: 'v4',
   accessKeyId: process.env.APP_AWS_ACCESS_KEY,
   secretAccessKey: process.env.APP_AWS_SECRET_KEY,
-  // region: 'eu-west-3'
+  region: 'eu-west-3'
 });
 
 
